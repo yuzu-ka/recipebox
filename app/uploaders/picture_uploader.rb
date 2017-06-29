ace5237225107570dc2d17673472bfa89e6bf90f
yuzu.ka <yuzuzu.ka.work@gmail.com>
2017-06-29 07:12:16 +0000

    add AWS setting

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,7 +8,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [250, 250]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+ 
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
