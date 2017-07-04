90f4a10f1c4931b5dbd47395c831eaef2132b081
yuzu.ka <yuzuzu.ka.work@gmail.com>
2017-07-04 05:58:42 +0000

    fixed S3 setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,15 +3,15 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['S3_ACCESS_KEY'],
     aws_secret_access_key: ENV['S3_SECRET_KEY'],
-    region: "ap-northeast-1"
+    region: ENV['S3_REGION']
     }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = 'specialrecipebox'
+        config.fog_directory  = ENV['S3_BUCKET']
         config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/specialrecipebox'
     when 'production'
-        config.fog_directory  = 'specialrecipebox'
+        config.fog_directory  = ENV['S3_BUCKET']
         config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/specialrecipebox'
     end
 end
