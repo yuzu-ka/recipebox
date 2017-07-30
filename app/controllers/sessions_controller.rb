53afc06db7d610f6f4a5ccebfb7a0d82d10ca01b
yuzu.ka <yuzuzu.ka.work@gmail.com>
2017-07-30 05:39:23 +0000

    fixed sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     password = params[:session][:password]
     if login(email, password)
       flash[:success] = 'ログインしました。'
-      redirect_to @user
+      redirect_to root_path
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
