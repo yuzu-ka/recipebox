d9b8e68b05ca62ba6595406b8e95a5ada61ef52e
yuzu.ka <yuzuzu.ka.work@gmail.com>
2017-07-30 09:59:41 +0000

    add followings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,18 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
   private
   
   def user_params
